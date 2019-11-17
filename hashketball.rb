7568b597d12c25238af7d31c51b9aba116cb3361
Anna Carey <anna@annajcarey.com>
2019-11-17 22:18:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,7 +185,7 @@ def player_stats(player_name)
   game.each do |one, team|
     team[:players].each do |player|
       if player[:player_name] == player_name
-        player.remove(:player_name)
+        player.delete(:player_name)
         return player 
       end 
   end
